0cb74916feede7945de1613542d1859e81b9c643
Robson Franklin <145121948+robsonnfranklin@users.noreply.github.com>
2024-11-18 23:17:26 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+   $('#mobile_btn').on('click',function () {
+        $('#mobile_menu').toggleclass('active');
+        $('#mobile_btn').find(i).toggleclass('fa-x');
+   })
+});
